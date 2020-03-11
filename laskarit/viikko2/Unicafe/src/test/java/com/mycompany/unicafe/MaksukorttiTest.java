9f8a638c3a9d1b8ad65133bc5341c7ce2b8103dd
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-03-11 21:16:17 +0200

    Lisätty testit Maksukortille

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,44 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(420);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortinSaldoOnAlussaOikein() {
+        assertEquals("saldo: 4.20", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(115);
+        assertEquals("saldo: 5.35", kortti.toString());
+    }
+    
+    @Test
+    public void kortiltaVoiOttaaRahaa() {
+        kortti.otaRahaa(350);
+        assertEquals(70, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        kortti.otaRahaa(500);
+        assertEquals(420, kortti.saldo());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosRahaaOnTarpeeksi() {
+        assertTrue(kortti.otaRahaa(420));
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosRahaaEiOleTarpeeksi() {
+        assertFalse(kortti.otaRahaa(421));
     }
 }
